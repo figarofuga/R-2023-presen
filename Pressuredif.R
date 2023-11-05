8f8a0f57dc1bd1bfd10c421b70baf52ae162e08a
figarofuga <61215157+figarofuga@users.noreply.github.com>
2023-11-05 12:37:53 +0000

    add lmer

--- Pressuredif.R
+++ Pressuredif.R
@@ -2,9 +2,8 @@ library(tidyverse)
 library(readxl)
 library(here)
 library(ggplot2)
-library(ggh4x)
-library(ggdist)
-library(blandr)
+library(lme4)
+library(parameters)
 
 dat <- readxl::read_xlsx(path = here::here("pressure_data.xlsx"))
 
@@ -12,30 +11,46 @@ dat <- readxl::read_xlsx(path = here::here("pressure_data.xlsx"))
 prepped_dat <- dat %>% 
   dplyr::mutate(valve = if_else(`ValveType 1;N 0;S` == 0, "Sapien", "Navitor"), 
                 catheter = if_else(catheter == 55555, NA_real_, catheter), 
-                diff = catheter - THVmeanPG_Post) 
+                diff = catheter - THVmeanPG_Post, 
+                echo = THVmeanPG_Post) %>% 
+  tidyr::drop_na()
 
 long_dat <- tidyr::pivot_longer(prepped_dat, 
-                      cols = c(catheter, THVmeanPG_Post), 
+                      cols = c(catheter, echo), 
                       names_to = "measure_device", 
-                      values_to = "pressure")
+                      values_to = "pressure") %>% 
+  dplyr::mutate(valve_device = paste0(valve, "_", measure_device), 
+                valve_device_fct = forcats::fct_relevel(valve_device, 
+                                                        c("Sapien_catheter", "Sapien_echo", "Navitor_catheter", "Navitor_echo")))
 
-fig <- ggpubr::ggboxplot(long_dat, x = "valve", 
-                  y = "pressure", fill = "valve",
-                  facet.by = "measure_device") + 
+
+# LMM model ---------------------------------------------------------------
+
+fit <- lmer(pressure ~ valve + measure_device + valve*measure_device + (1|PatientIdOld), data = long_dat)
+parameters::model_parameters(fit)
+
+# make figure -------------------------------------------------------------
+
+
+
+fig <- ggpubr::ggboxplot(long_dat, x = "valve_device_fct", 
+                  y = "pressure", fill = "valve_device_fct") + 
   scale_y_continuous(limits = c(0, 35)) +
   ggpubr::stat_compare_means(label.y = 32)
 
-fig2 <- ggpubr::ggboxplot(long_dat, x = "measure_device", 
-                         y = "pressure", 
-                         fill = "measure_device", facet.by = "valve") + 
-  scale_y_continuous(limits = c(0, 35)) +
-  ggpubr::stat_compare_means(label.y = 32)
 
-ggpubr::ggpaired(long_dat, x = "measure_device", 
-                  y = "pressure", 
-                  fill = "measure_device", facet.by = "valve") + 
-  scale_y_continuous(limits = c(0, 35)) +
-  ggpubr::stat_compare_means(label.y = 32)
+
+# fig2 <- ggpubr::ggboxplot(long_dat, x = "measure_device", 
+#                          y = "pressure", 
+#                          fill = "measure_device", facet.by = "valve") + 
+#   scale_y_continuous(limits = c(0, 35)) +
+#   ggpubr::stat_compare_means(label.y = 32)
+# 
+# ggpubr::ggpaired(long_dat, x = "measure_device", 
+#                   y = "pressure", 
+#                   fill = "measure_device", facet.by = "valve") + 
+#   scale_y_continuous(limits = c(0, 35)) +
+#   ggpubr::stat_compare_means(label.y = 32)
 
 
 
